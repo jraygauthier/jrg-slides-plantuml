5ddfeb0737c2d88a16846d0e481b36867710acd7
Raymond Gauthier <jraygauthier@gmail.com>
2020-01-29 23:16:49 -0500

    Update pmw pin

--- .nix/pinned-pandoc-md-wiki-src.nix
+++ .nix/pinned-pandoc-md-wiki-src.nix
@@ -2,9 +2,9 @@
 
 let
   pinnedSrc = builtins.fetchTarball {
-    url = "https://github.com/jraygauthier/pandoc-md-wiki/archive/b71523837dc21836d69936022830cbd834d6d8f6.tar.gz";
+    url = "https://github.com/jraygauthier/pandoc-md-wiki/archive/11cba928401b7c8e436602c719e58424b7baff0b.tar.gz";
     # Get this info from the output of: `nix-prefetch-url --unpack $url` where `url` is the above.
-    sha256 = "0fdj4raapw2kbhn1nlmhpkp2xkq8j35fjlclhzr4sfz5979kgzgm";
+    sha256 = "04257a6acyww30kwmvbmqw44gshs5y2jy9f5yci9kx13xb5r3k3v";
   };
 in
 
